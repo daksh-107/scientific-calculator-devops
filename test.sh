97e6d41b671b2c6ae5317b39f9f33546091cab90
Daksh Minda <dakshminda10@gmail.com>
2026-03-07 11:48:31 +0000

    Fix automated testing for Jenkins

--- test.sh
+++ test.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
+echo "Compiling calculator..."
 gcc calculator.c -o calculator -lm
 
-echo "Testing addition..."
-echo -e "1\n5\n3\n7\n" | ./calculator
+echo "Running automated test..."
 
-echo "Testing square root..."
-echo -e "6\n16\n7\n" | ./calculator
+printf "1\n5\n7\n10\n" | ./calculator
 
-echo "Tests completed."
+echo "Test finished successfully"
